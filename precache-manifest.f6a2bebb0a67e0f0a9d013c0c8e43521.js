711eefba070e3710b296aa9a1e89a64ce6763315
RoshanBanu23 <aasva.nikil@gmail.com>
2023-02-11 15:51:05 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f6a2bebb0a67e0f0a9d013c0c8e43521.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa344b45705713daff680b017aa8eba",
+    "url": "/NikilChrist/model/index.html"
+  },
+  {
+    "revision": "2bb2dca10be47c1374c3",
+    "url": "/NikilChrist/model/static/js/2.b59d03cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NikilChrist/model/static/js/2.b59d03cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1079b9dccdbff29d885f",
+    "url": "/NikilChrist/model/static/js/main.e466d3af.chunk.js"
+  },
+  {
+    "revision": "1b52eb3700b47094ee7b",
+    "url": "/NikilChrist/model/static/js/runtime-main.480d50a4.js"
+  },
+  {
+    "revision": "31387364efd5048127621a501e3fa87c",
+    "url": "/NikilChrist/model/static/media/2.31387364.jpg"
+  },
+  {
+    "revision": "ec4ad7c5c25e66268bf0c64cc23a05c2",
+    "url": "/NikilChrist/model/static/media/3.ec4ad7c5.jpg"
+  },
+  {
+    "revision": "e7ba35fdc81903921df0ccb9817ff565",
+    "url": "/NikilChrist/model/static/media/eljefe.e7ba35fd.jpg"
+  },
+  {
+    "revision": "2f3c2cbd9f09b9b1cbb722c7962fab41",
+    "url": "/NikilChrist/model/static/media/overview.2f3c2cbd.jpg"
+  },
+  {
+    "revision": "1c3686060ee7a718a35025996f217707",
+    "url": "/NikilChrist/model/static/media/product1.1c368606.png"
+  },
+  {
+    "revision": "97a2d662de3ed5c171f087fc38b1e047",
+    "url": "/NikilChrist/model/static/media/product2.97a2d662.png"
+  },
+  {
+    "revision": "a710eca7af473779c25433b10e21e702",
+    "url": "/NikilChrist/model/static/media/productone.a710eca7.png"
+  },
+  {
+    "revision": "a55a08e17c510b897902b89eea627d6a",
+    "url": "/NikilChrist/model/static/media/producttwo.a55a08e1.png"
+  }
+]);
